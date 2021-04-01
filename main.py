64e02d2b2d76d8787304b703f3582930bff99f83
Beyer Benjamin <813615@stma.k12.mn.us>
2021-04-01 10:11:32 -0500

    closertoten #1

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def close10(a, b)
-    if abs(a) == 
+def close10(a, b):
+    if abs(a-10) < abs(b-10):
+        return a
+    elif abs(a-10) > abs(b-10):
+        return b
+    else:
+        return 0
 
-
+print (close10(8,8))
 
 
